d8f5ed0dd481256e9f1e0f60a1443884792d5857
Est-1y <133119207+Est-1y@users.noreply.github.com>
2024-06-26 11:47:08 +0300

    Create db_storage.py

---
+++ models/engine/db_storage.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+""" module new class for sqlAlchemy """
+
+from sqlalchemy.orm import sessionmaker, scoped_session
+from os import getenv
+from sqlalchemy import (create_engine)
+from models.base_model import Base
+from sqlalchemy.ext.declarative import declarative_base
+from models.state import State
+from models.user import User
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+
+
+class DBStorage:
+    """ creating tables"""
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        user = getenv("HBNB_MYSQL_USER")
+        passwd = getenv("HBNB_MYSQL_PWD")
+        db = getenv("HBNB_MYSQL_DB")
+        host = getenv("HBNB_MYSQL_HOST")
+        env = getenv("HBNB_ENV")
+
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(user, passwd, host, db),
+                                      pool_pre_ping=True)
+
+        if env == "test":
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """returning a dictionary
+        """
+        dic = {}
+        if cls:
+            if type(cls) is str:
+                cls = eval(cls)
+            query = self.__session.query(cls)
+            for elem in query:
+                key = "{}.{}".format(type(elem).__name__, elem.id)
+                dic[key] = elem
+        else:
+            lista = [State, City, User, Place, Review, Amenity]
+            for clase in lista:
+                query = self.__session.query(clase)
+                for elem in query:
+                    key = "{}.{}".format(type(elem).__name__, elem.id)
+                    dic[key] = elem
+        return (dic)
+
+    def new(self, obj):
+        """adding a new element
+        """
+        self.__session.add(obj)
+
+    def save(self):
+        """save
+        """
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """deleting elements
+        """
+        if obj:
+            self.session.delete(obj)
+
+    def reload(self):
+        """
+        """
+        Base.metadata.create_all(self.__engine)
+        sec = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(sec)
+        self.__session = Session()
+
+    def close(self):
+        """
+        """
+        self.__session.close()
