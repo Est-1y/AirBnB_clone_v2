4c44f45f10bf5e788d16b1a6d2b0f630a922113b
Est-1y <133119207+Est-1y@users.noreply.github.com>
2024-06-27 12:34:29 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" State Module, amenity class """
+"""State Module, amenity class."""
+
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 from models.place import place_amenity
@@ -7,6 +8,6 @@ from sqlalchemy import Column, String
 
 
 class Amenity(BaseModel, Base):
-     __tablename__ = "amenities"
+    __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
     place_amenities = relationship("Place", secondary=place_amenity)
