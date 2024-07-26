fd9995af0b7d0f626edc9a833060fa29d5328fe3
Est-1y <133119207+Est-1y@users.noreply.github.com>
2024-07-26 13:39:10 +0300

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,6 +6,8 @@ listens on 0.0.0.0, port 5000.
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from models import *
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
@@ -21,9 +23,10 @@ def states_list():
     """
     Printing an HTML page with a list of all State objects in DBStorage
     """
-    states = storage.all(State).values()
-    return render_template("7-states_list.html", states=states)
+    sorted_states = sorted(list(storage.all("State").
+                                values()), key=lambda x: x.name)
 
+    return render_template('7-states_list.html', states=sorted_states)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
